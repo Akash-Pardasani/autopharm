a19301b2bbd9971426dd290e1617d9056ad0b36f
Akash-Pardasani <smartavi004@gmail.com>
2018-06-11 21:53:02 +0530

    Update QtMenu.py

--- QtMenu.py
+++ QtMenu.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt4 import QtGui, QtCore
 import os
+import DB_manager
 
 mainPath = ('main240.png')
 homePath = ('home.png')
@@ -24,6 +25,22 @@ resetAfter = ('resetafter.png')
 
 sheetFields = {'Item':[], 'Code':[], 'Item Type':[], 'Quantity':[], 'Batch no.':[], 'Expiry':[], 'Price':[], 'Discount %':[], 'VAT %':[], 'Amount':[]}
 sheetHeaders = ['Item',  'Quantity', 'Code', 'Item Type', 'Batch no.', 'Expiry', 'Price', 'Discount %', 'VAT %', 'Amount']
+
+dbu = DB_manager.DatabaseUtility('MedDB', 'DemoDB')
+dbu.AddEntryToTable('M1', 1, 'MED', 11, '2018-06-20', 20, 18, 1, 1)
+dbu.AddEntryToTable('M2', 2, 'MED', 12, '2018-06-21', 25, 18, 2, 2)
+dbu.AddEntryToTable('M3', 3, 'SYP', 13, '2018-06-22', 30, 18, 3, 3)
+dbu.AddEntryToTable('M4', 4, 'SYP', 14, '2018-06-23', 40, 18, 4, 4)
+dbu.AddEntryToTable('M5', 5, 'FMCG', 15, '2018-06-24', 50, 18, 5, 5)
+
+gpname = "Default"
+gdname = "Default"
+ganame = "Default"
+gpaddr = "Default"
+gpcont = "Default"
+gtamt = 100
+gtdisc = 20
+gttax = 18
 
 class MainLogo(QtGui.QLabel):
 	def __init__(self, parent = None):
@@ -130,7 +147,7 @@ class Home(QtGui.QWidget):
 		element1 = QtGui.QWidget(self)
 		element1.setLayout(hbox1)
 		hbox1.addSpacing(455)
-		hbox1.addWidget(self.logo)
+		#hbox1.addWidget(self.logo)
 			
 		hbox2 = QtGui.QHBoxLayout()
 		element2 = QtGui.QWidget(self)
@@ -184,7 +201,7 @@ class Home(QtGui.QWidget):
 		fileMenu = self.menu.addMenu('&File')
 		fileMenu.addAction(exitAction)
 			
-		self.setGeometry(100, 100, 1200, 800)
+		self.setGeometry(50, 50, 600, 400)
 		self.setWindowTitle('AxesPro')
 		self.setFocus()
 		self.show()
@@ -198,7 +215,10 @@ class Prescription(QtGui.QWidget):
 		self.setStyleSheet("background-color: white;")
 	
 		alias = QtGui.QWidget(self)
-
+		self.count = -1
+		self.totamt = 0
+		self.tottax = 0
+		self.totdisc = 0
 		vbox = QtGui.QVBoxLayout()
 		self.setLayout(vbox)
  
@@ -242,43 +262,45 @@ class Prescription(QtGui.QWidget):
 		self.docID = QtGui.QLineEdit()
 		#self.docID.setFixedWidth(200)
 
-		self.addMed = QtGui.QPushButton('Add', self)
+		self.adder = QtGui.QPushButton('Add', self)
+		self.adder.clicked.connect(self.addMed)
 
 		self.cancelOrder = QtGui.QLabel(self)
 		self.pixmap = QtGui.QPixmap(cancelPath)
 		self.cancelOrder.setPixmap(self.pixmap)
 
-		self.l = QtGui.QLabel()
-		self.resetOrder = ResetLogo(self.l)
+		self.l1 = QtGui.QLabel()
+		self.resetOrder = ResetLogo(self.l1)
 
-		self.l = QtGui.QLabel()
-		self.saveOrder = SaveLogo(self.l)
+		self.l2 = QtGui.QLabel()
+		self.saveOrder = SaveLogo(self.l2)
 
-		self.l = QtGui.QLabel()
-		self.proceed = NextLogo(self.l)
+		self.l3 = QtGui.QLabel()
+		self.proceed = QtGui.QPushButton('Next', self)
 
 
-		table = QtGui.QTableWidget(self)
-		table.setRowCount(15)
-		table.setColumnCount(10)
+		self.table = QtGui.QTableWidget(self)
+		self.table.setRowCount(15)
+		self.table.setColumnCount(10)
 		headers = []
 		for key, value in sheetFields.items():
 			headers.append(key)
 			for m, item in enumerate(sheetFields[key]):
 				newitem = QtGui.QTableWidgetItem(item)
-				table.setItem(m, n, newitem)
-		table.setHorizontalHeaderLabels(sheetHeaders)		
-		table.setColumnWidth(0, 200)
-		table.setColumnWidth(5, 150)
-		table.setColumnWidth(9, 100)
-		table.setColumnWidth(2, 150)
-		table.setColumnWidth(4, 105)
+				self.table.setItem(m, n, newitem)
+		self.table.setHorizontalHeaderLabels(sheetHeaders)		
+		self.table.setColumnWidth(0, 200)
+		self.table.setColumnWidth(5, 150)
+		self.table.setColumnWidth(9, 100)
+		self.table.setColumnWidth(2, 150)
+		self.table.setColumnWidth(4, 105)
+		self.table.setItem(14, 8, QtGui.QTableWidgetItem("Total Amount "))
 		
 		hbox1 = QtGui.QHBoxLayout()
 		element1 = QtGui.QWidget(self)
 		element1.setLayout(hbox1)
 		#hbox1.addSpacing(40)
-		hbox1.addWidget(table)
+		hbox1.addWidget(self.table)
 		
 		hbox2 = QtGui.QHBoxLayout()
 		element2 = QtGui.QWidget(self)
@@ -301,9 +323,9 @@ class Prescription(QtGui.QWidget):
 		element4 = QtGui.QWidget(self)
 		element4.setLayout(hbox4)
 		hbox4.addSpacing(900)
-		#hbox4.addWidget(self.resetOrder)
-		#hbox4.addWidget(self.saveOrder)
-		#hbox4.addWidget(self.proceed)
+		hbox4.addWidget(self.resetOrder)
+		hbox4.addWidget(self.saveOrder)
+		hbox4.addWidget(self.proceed)
 
 		hbox5 = QtGui.QHBoxLayout()
 		element5 = QtGui.QWidget(self)
@@ -324,7 +346,7 @@ class Prescription(QtGui.QWidget):
 		hbox6.addWidget(self.Exp)
 		hbox6.addWidget(self.Dosage)
 		hbox6.addWidget(self.Discount)
-		hbox6.addWidget(self.addMed)
+		hbox6.addWidget(self.adder)
 
 		grid1 = QtGui.QGridLayout()
 		element7 = QtGui.QWidget(self)
@@ -365,19 +387,75 @@ class Prescription(QtGui.QWidget):
 		fileMenu = self.menu.addMenu('&File')
 		#fileMenu.addAction(exitAction)
 			
-		self.setGeometry(100, 100, 1200, 800)
+		self.setGeometry(50, 50, 600, 400)
 		self.setWindowTitle('AxesPro')
 		self.show()
+
 
 	def updateTime(self):
 		current = QtCore.QDateTime.currentDateTime().toString()
 		self.time_label.setText(current)
 
+	def addMed(self):
+		global gpname
+		gpname = str(self.patientID.text())
+		#print(str(self.patientID.text()))
+		#print(gpname)
+		global ganame 
+		ganame = str(self.empID.text())
+		global gdname
+		gdname = str(self.docID.text())
+		global gpaddr
+		gpaddr = str(self.patientAddress.text())
+		global gpcont
+		gpcont = str(self.patientContact.text())
+		self.count += 1
+		med = str(self.itemID.text())
+		cmd = "SELECT * FROM DemoDB WHERE Item_Name = '" + med + "'"
+		res1 = dbu.RunCommand(cmd)
+		res = res1[0]
+		#print res
+		tname = res[0]
+		tcode = res[1]
+		ttype = res[2]
+		tbno = res[3]
+		texp = res[4]
+		tprice = int(res[5])
+		tvat = res[6]
+		x = res[7]
+		y = res[8]
+		tqty = int(self.Qty.text())
+		tdisc = str(self.Discount.text())
+		tdos = str(self.Dosage.text())
+		tamt = tqty*tprice
+		#self.table.setItem()
+		fin = []
+		fin.append(tname)
+		fin.append(tqty)
+		fin.append(tcode)
+		fin.append(ttype)
+		fin.append(tbno)
+		fin.append(texp)
+		fin.append(tprice)
+		fin.append(tdisc)
+		fin.append(tvat)
+		fin.append(tamt)
+		#print tamt
+		self.totamt += tamt
+		for i in xrange(0,10):
+			self.table.setItem(self.count, i, QtGui.QTableWidgetItem(str(fin[i])))
+		self.table.setItem(14, 9, QtGui.QTableWidgetItem(str(self.totamt)))	
+		global gtamt
+		gtamt = str(self.totamt)
+		global gtdisc
+		gtdisc = str(self.totdisc)
+		global gttax
+		gttax = str(self.tottax)
 
 class Inventory(QtGui.QWidget):
 	def __init__(self, parent = None):
 		super(Inventory, self).__init__(parent)
-		self.setGeometry(100,100,1200,800)
+		self.setGeometry(100,100,600,400)
 		self.setStyleSheet("background-color: white;")
 
 		vbox = QtGui.QVBoxLayout()
@@ -457,7 +535,7 @@ class Inventory(QtGui.QWidget):
 		fileMenu = self.menu.addMenu('&File')
 		#fileMenu.addAction(exitAction)
 			
-		self.setGeometry(100, 100, 1200, 800)
+		self.setGeometry(50, 50, 600, 400)
 		self.setWindowTitle('Stock')
 		self.show()
 
@@ -466,12 +544,131 @@ class Inventory(QtGui.QWidget):
 		self.expiry.setText("Today")
 		self.refill.setText("Tomorrow")	
 
+class confirmPrescription(QtGui.QWidget):
+	def __init__(self, parent = None):
+		super(confirmPrescription, self).__init__(parent)
+		self.setGeometry(50, 50, 600, 400)
+		self.setStyleSheet("background-color: white;")
+
+		vbox = QtGui.QVBoxLayout()
+		self.setLayout(vbox)
+
+		self.pName = QtGui.QLabel("Patient Name: ")
+		self.confPName = QtGui.QLabel()
+		self.aName = QtGui.QLabel("Attendant Name: ")
+		self.confAName = QtGui.QLabel()
+		self.dName = QtGui.QLabel("Doctor Name: ")
+		self.confDName = QtGui.QLabel()
+		self.pAddr = QtGui.QLabel("Patient Address: ")
+		self.confPAddr = QtGui.QLabel()
+		self.pContact = QtGui.QLabel("Patient Contact: ")
+		self.confPCnct = QtGui.QLabel()
+		self.Amt = QtGui.QLabel("Total Amount: ")
+		self.confAmt = QtGui.QLabel()
+		self.Disc = QtGui.QLabel("Total Discount: ")
+		self.confDisc = QtGui.QLabel()
+		self.Tax = QtGui.QLabel("Tax %: ")
+		self.confTax = QtGui.QLabel()
+		self.amtPay = QtGui.QLabel("Net Amount Payable: ")
+		self.confAmtPay = QtGui.QLabel()
+		self.payMode = QtGui.QLabel("Payment Mode")
+
+		self.cash = QtGui.QRadioButton("Cash")
+		self.debit = QtGui.QRadioButton("Debit Card")
+		self.credit = QtGui.QRadioButton("Credit Card")
+
+		self.printPres = QtGui.QLabel()
+		pixmap = QtGui.QPixmap(printPath)
+		self.printPres.setPixmap(pixmap)
+
+		hbox1 = QtGui.QHBoxLayout()
+		element1 = QtGui.QWidget(self)
+		element1.setLayout(hbox1)
+		hbox1.addWidget(self.pName)
+		hbox1.addWidget(self.confPName)
+		hbox1.addSpacing(300)
+		hbox1.addWidget(self.aName)
+		hbox1.addWidget(self.confAName)
+
+		hbox2 = QtGui.QHBoxLayout()
+		element2 = QtGui.QWidget(self)
+		element2.setLayout(hbox2)
+		hbox2.addWidget(self.pAddr)
+		hbox2.addWidget(self.confPAddr)
+		hbox2.addSpacing(300)
+		hbox2.addWidget(self.dName)
+		hbox2.addWidget(self.confDName)
+
+		hbox3 = QtGui.QHBoxLayout()
+		element3 = QtGui.QWidget(self)
+		element3.setLayout(hbox3)
+		hbox3.addWidget(self.pContact)
+		hbox3.addWidget(self.confPCnct)
+
+		hbox4 = QtGui.QHBoxLayout()
+		element4 = QtGui.QWidget(self)
+		element4.setLayout(hbox4)
+		hbox4.addWidget(self.Amt)
+		hbox4.addWidget(self.confAmt)
+
+		hbox5 = QtGui.QHBoxLayout()
+		element5 = QtGui.QWidget(self)
+		element5.setLayout(hbox5)
+		hbox5.addWidget(self.Disc)
+		hbox5.addWidget(self.confDisc)
 		
+		hbox6 = QtGui.QHBoxLayout()
+		element6 = QtGui.QWidget(self)
+		element6.setLayout(hbox6)
+		hbox6.addWidget(self.Tax)
+		hbox6.addWidget(self.confTax)
+		
+		hbox7 = QtGui.QHBoxLayout()
+		element7 = QtGui.QWidget(self)
+		element7.setLayout(hbox7)
+		hbox7.addWidget(self.amtPay)
+		hbox7.addWidget(self.confAmtPay)
+		
+		hbox8 = QtGui.QHBoxLayout()
+		element8 = QtGui.QWidget(self)
+		element8.setLayout(hbox8)
+		hbox8.addWidget(self.payMode)
+
+		hbox9 = QtGui.QHBoxLayout()
+		element9 = QtGui.QWidget(self)
+		element9.setLayout(hbox9)
+		hbox9.addWidget(self.cash)
+		hbox9.addSpacing(50)
+		hbox9.addWidget(self.debit)
+		hbox9.addSpacing(50)
+		hbox9.addWidget(self.credit)
+
+		hbox10 = QtGui.QHBoxLayout()
+		element10 = QtGui.QWidget(self)
+		element10.setLayout(hbox10)
+		hbox10.addSpacing(950)
+		hbox10.addWidget(self.printPres)
+
+		vbox.addWidget(element1)
+		vbox.addWidget(element2)
+		vbox.addWidget(element3)
+		vbox.addWidget(element4)
+		vbox.addWidget(element5)
+		vbox.addWidget(element6)
+		vbox.addWidget(element7)
+		vbox.addWidget(element8)
+		vbox.addWidget(element9)
+		vbox.addWidget(element10)
+		self.setWindowTitle('Confirm')
+		self.show()		
+		
+		
+
 
 class MainWindow(QtGui.QMainWindow):
 	def __init__(self, parent = None):
 		super(MainWindow, self).__init__(parent)
-		self.setGeometry(100, 100, 1200, 800)
+		self.setGeometry(50, 50, 600, 400)
 		self.setStyleSheet("background-color: white;")
 		self.startHome()
 
@@ -492,6 +689,7 @@ class MainWindow(QtGui.QMainWindow):
 		self.setWindowTitle('Fill New Prescription')
 		self.setCentralWidget(self.window)
 		self.window.homeAction.triggered.connect(self.startHome)
+		self.window.proceed.clicked.connect(self.confPres)
 		self.setStyleSheet("background-color: white;")
 		self.show()	
 
@@ -502,6 +700,25 @@ class MainWindow(QtGui.QMainWindow):
 		self.window.homeAction.triggered.connect(self.startHome)
 		self.setStyleSheet("background-color: white;")
 		self.show()			
+
+	def confPres(self):
+		QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+		self.window = confirmPrescription(self)
+		self.setWindowTitle('Confirm Prescription')
+		self.setCentralWidget(self.window)
+		self.setStyleSheet("background-color: white;")
+		self.window.confPName.setText(str(gpname))
+		#print(gpname)
+		self.window.confAName.setText((str(ganame)))
+		self.window.confDName.setText((str(gdname)))
+		self.window.confPAddr.setText((str(gpaddr)))
+		self.window.confPCnct.setText((str(gpcont)))
+		self.window.confAmt.setText((str(gtamt)))
+		self.window.confDisc.setText((str(gtdisc)))
+		self.window.confTax.setText((str(gttax)))
+		apay = int(gtamt) - int(gtdisc) + int(gttax)
+		self.window.confAmtPay.setText(str(apay))
+		self.show()
 
 
 def main():
